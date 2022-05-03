e69c572a488172834d2401a0bdc7cf95f08572f7
Jiwei Liu <aixueer4ever@gmail.com>
2022-05-03 07:53:36 -0700

    test

--- test/test.py
+++ test/test.py
@@ -1,8 +1,14 @@
 import session_split
 s = [1, 1, 2, 2, 2]
 x = [3, 5, 6, 5, 7]
-y = [4,8,8,9,9]
+y = [6, 6, 8, 8, 8]
 X,Y = session_split.split_session(s,x,y)
+print('session')
+print(s)
+print('items viewed')
+print(x)
+print('items purchased')
+print(y)
 print('X')
 print(X)
 print('Y')

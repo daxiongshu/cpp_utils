c8f4d8d7c67efd5952842b03d674f0d8a56393bc
Jiwei Liu <aixueer4ever@gmail.com>
2022-05-03 07:34:47 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import Extension, setup, find_packages
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    # create closure for deferred import
+    def cythonize(*args, ** kwargs):
+        from Cython.Build import cythonize
+        return cythonize(*args, ** kwargs)
+
+setup(
+    name="session_split",
+    author='jiwei',
+    setup_requires=[
+        'cython',
+    ],
+    ext_modules=cythonize([
+        Extension(
+            'session_split',
+            sorted(['python/session.pyx','cpp/src/session.cpp']),
+            include_dirs=[
+                # c/cpp defination includes folder path
+                'cpp/include'
+            ],
+            extra_link_args=[],  # like '-lX11'
+            language='c++'
+        ),
+        
+    ]
+    ),
+    zip_safe=False,
+)
